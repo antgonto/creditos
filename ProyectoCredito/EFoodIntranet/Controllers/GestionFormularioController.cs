43553b5e7cbebb4e12fdcd3d69d06f340edc213b
LuisitoCrew19 <45248187+LuisitoCrew19@users.noreply.github.com>
2020-08-18 17:17:03 -0600

    GestionFormulario

---
+++ ProyectoCredito/EFoodIntranet/Controllers/GestionFormularioController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL;
+
+namespace EFoodIntranet.Controllers
+{
+    public class GestionFormularioController
+    {
+        public string Get(int id)
+        {
+            return new GestionFormulario().carga_GestionFormulario(id);
+        }
+
+
+        // POST api/values      
+        public string Post([FromBody] GestionFormulario gestionFormulario)
+        {
+            return gestionFormulario.agregar_gestionformulario("Insertar") ? " Se guardo el formulario" : "No se guardo el formulario.";
+
+        }
+
+        //PUT api/values/5      
+        public string Put(string id, [FromBody] GestionFormulario gestionFormulario)
+        {
+            return gestionFormulario.modificar_gestionformulario("Actualizar") ? "Se actualizo el formulario" :
+               "No se actualizo el formualrio";
+        }
+
+    }
+
+}
